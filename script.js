25a9aa7f7038fb24dae440610b37e21556306d5b
EnsarBaltas <ensarbaltas@gmail.com>
2020-05-11 19:30:06 +0200

    Largest Number in Arrays

--- script.js
+++ script.js
@@ -6,5 +6,15 @@ function toCelsius(f) {
 }
 document.getElementById("Celcius").innerHTML = "temperature : "+  50 + " Fahreneit, " + toCelsius(50) + " degre "
 
+function handler1() {
+  alert('Thanks!');
+};
 
+function handler2() {
+  alert('Thanks again!');
+}
+
+elem.onclick = () => alert("Hello");
+elem.addEventListener("click", handler1); // Thanks!
+elem.addEventListener("click", handler2); // Thanks again!
 
